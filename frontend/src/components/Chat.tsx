e462f1b2304a3ed7c58929972e5b49d33220fcec
Maganez Filho <maganezfilho@gmail.com>
2022-08-28 11:26:18 -0300

    feat(app): initial commit

---
+++ frontend/src/components/Chat.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+import { Socket } from 'socket.io-client';
+
+type Message = {
+	room: string;
+	author: string;
+	message: string;
+	time: string;
+};
+
+interface ChatProps {
+	socket: Socket;
+	username: string;
+	room: string;
+}
+
+export function Chat({ socket, username, room }: ChatProps) {
+	const [currentMessage, setCurrentMessage] = useState('');
+	const [messages, setMessages] = useState<Message[]>([]);
+
+	const sendMessage = () => {
+		if (currentMessage) {
+			const newMessage: Message = {
+				room: room,
+				author: username,
+				message: currentMessage,
+				time:
+					new Date(Date.now()).getHours() +
+					':' +
+					new Date(Date.now()).getMinutes(),
+			};
+
+			socket.emit('send_message', newMessage);
+
+			setMessages(message => [...message, newMessage]);
+			setCurrentMessage('');
+		}
+	};
+
+	useEffect(() => {
+		socket.on('receive_message', (newMessage: Message) =>
+			setMessages(message => [...message, newMessage])
+		);
+	}, [socket]);
+
+	return (
+		<div className="chat-window">
+			<div className="chat-header">
+				<p>Live Chat</p>
+			</div>
+
+			<div className="chat-body">
+				<ScrollToBottom className="message-container">
+					{messages.map(message => {
+						return (
+							<div
+								className="message"
+								key={message.message}
+								id={username === message.author ? 'you' : 'other'}
+							>
+								<div>
+									<div className="message-content">
+										<p>{message.message}</p>
+									</div>
+									<div className="message-meta">
+										<p id="time">{message.time}</p>
+										<p id="author">{message.author}</p>
+									</div>
+								</div>
+							</div>
+						);
+					})}
+				</ScrollToBottom>
+			</div>
+
+			<div className="chat-footer">
+				<input
+					type="text"
+					value={currentMessage}
+					placeholder="Type your message here"
+					onChange={({ target: { value } }) => setCurrentMessage(value)}
+					onKeyPress={({ key }) => key === 'Enter' && sendMessage()}
+				/>
+				<button onClick={sendMessage}>&#9658;</button>
+			</div>
+		</div>
+	);
+}
---
+++ types.ts
@@ -0,0 +1,6 @@
+export type Message = {
+	room: string;
+	author: string;
+	message: string;
+	time: string;
+};
